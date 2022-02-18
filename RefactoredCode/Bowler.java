491bccc06b200d0ae15068bec59bcf0163f1f307
Akshay M <akshayknr7@gmail.com>
2022-02-19 00:38:33 +0530

    refactored bowler file

--- RefactoredCode/Bowler.java
+++ RefactoredCode/Bowler.java
@@ -1,21 +1,3 @@
-/*
- * Bowler.java
- *
- * Version:
- *     $Id$
- *
- * Revisions:
- *     $Log: Bowler.java,v $
- *     Revision 1.3  2003/01/15 02:57:40  ???
- *     Added accessors and and equals() method
- *
- *     Revision 1.2  2003/01/12 22:23:32  ???
- *     *** empty log message ***
- *
- *     Revision 1.1  2003/01/12 19:09:12  ???
- *     Adding Party, Lane, Bowler, and Alley.
- *
- */
 
 /**
  *  Class that holds all bowler info
@@ -29,20 +11,18 @@ public class Bowler {
     private String email;
 
     public Bowler( String nick, String full, String mail ) {
-	nickName = nick;
-	fullName = full;
-  	email = mail;
+		nickName = nick;
+		fullName = full;
+		email = mail;
     }
 
 
     public String getNickName() {
-
         return nickName;  
-
     }
 
 	public String getFullName ( ) {
-			return fullName;
+		return fullName;
 	}
 	
 	public String getNick ( ) {
@@ -54,16 +34,6 @@ public class Bowler {
 	}
 	
 	public boolean equals ( Bowler b) {
-		boolean retval = true;
-		if ( !(nickName.equals(b.getNickName())) ) {
-				retval = false;
-		}
-		if ( !(fullName.equals(b.getFullName())) ) {
-				retval = false;
-		}	
-		if ( !(email.equals(b.getEmail())) ) {
-				retval = false;
-		}
-		return retval;
+		return nickName.equals(b.getNickName()) && fullName.equals(b.getFullName()) && email.equals(b.getEmail());
 	}
 }
